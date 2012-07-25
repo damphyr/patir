19b42da11c686edbfd7eba142c76b24cac44e2a6
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2012-07-25 15:42:30 +0300

    Bumped version, added Travis CI config

--- lib/patir/base.rb
+++ lib/patir/base.rb
@@ -6,7 +6,7 @@ module Patir
   module Version
     MAJOR=0
     MINOR=7
-    TINY=2
+    TINY=3
     STRING=[ MAJOR, MINOR, TINY ].join( "." )  	
   end
   #Error thrown usually in initialize methods when missing required parameters
