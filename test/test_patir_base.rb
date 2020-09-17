ce48fb965f3500d745094b6bd25e7a9c4cdd6186
markuspg <markuspg@users.noreply.github.com>
2020-09-17 11:00:10 +0200

    Test Patir::Version

--- test/test_patir_base.rb
+++ test/test_patir_base.rb
@@ -25,3 +25,16 @@ class TestBase<Minitest::Test
   end
   
 end
+
+##
+# Test Patir::Version
+class TestVersion < Minitest::Test
+  ##
+  # Verify that the version data is correctly set
+  def test_version_data
+    assert_equal(0, Patir::Version::MAJOR)
+    assert_equal(9, Patir::Version::MINOR)
+    assert_equal(0, Patir::Version::TINY)
+    assert_equal('0.9.0', Patir::Version::STRING)
+  end
+end
