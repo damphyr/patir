c820946085093cf6c78be0b03d43c503fe1e4efb
markuspg <markuspg@users.noreply.github.com>
2020-09-17 14:36:36 +0200

    Test Patir.setup_logger

--- test/test_patir_base.rb
+++ test/test_patir_base.rb
@@ -4,26 +4,58 @@ $:.unshift File.join(File.dirname(__FILE__),"..","lib")
 require "minitest/autorun"
 require 'patir/base.rb'
 
-class TestBase<Minitest::Test
-  TEMP_LOG="temp.log"
+##
+# Test Patir
+class TestPatir < Minitest::Test
+  ##
+  # A temporary log file for testing
+  TEMP_LOG = 'temp.log'.freeze
+
+  ##
+  # Clean-up steps after each of the Patir test cases
   def teardown
-    #clean up 
     File.delete(TEMP_LOG) if File.exist?(TEMP_LOG)
   end
-  
-  #This is not actually testing anything meaningfull but can be expanded when we learn more about 
-  #the logger
-  def test_setup_logger
-    logger=Patir.setup_logger
-    refute_nil(logger)
-    logger=Patir.setup_logger(nil,:silent)
-    refute_nil(logger)
-    logger=Patir.setup_logger("temp.log",:silent)
-    refute_nil(logger)
-    assert(File.exist?(TEMP_LOG), "Log file not created")
-    logger.close
+
+  ##
+  # Verify that the default logger setup works correctly
+  def test_setup_logger_default
+    logger = Patir.setup_logger
+    assert_equal(Logger::INFO, logger.level)
+    assert_instance_of(Patir::PatirLoggerFormatter, logger.formatter)
+    out, = capture_subprocess_io do
+      logger.debug('Test')
+      logger.warn('Oh oh')
+    end
+    puts "out: #{out}"
+    assert_match(/\[\d{8} \d\d:\d\d:\d\d\]  WARN: Oh oh\n/, out)
   end
-  
+
+  ##
+  # Verify that Patir.setup_logger logs to the correct file
+  def test_setup_logger_file
+    logger = Patir.setup_logger(TEMP_LOG, nil)
+    logger.close
+    assert(File.exist?(TEMP_LOG))
+  end
+
+  ##
+  # Verify that Patir.setup_logger correctly handles mode parameters
+  def test_setup_logger_mode
+    [[:debug, [Logger::DEBUG], 'DEBUG'],
+     [:mute, [Logger::DEBUG, Logger::WARN, Logger::FATAL], 'FATAL'],
+     [:silent, [Logger::INFO, Logger::WARN], ' WARN']].each do |data|
+      logger = Patir.setup_logger(nil, data[0])
+      out, = capture_subprocess_io do
+        data[1].each do |severity|
+          logger.log(severity, 'Test Message')
+        end
+      end
+      match = /\[\d{8} \d\d:\d\d:\d\d\] (DEBUG|FATAL| WARN): Test Message\n/.match(out)
+      refute_nil(match)
+      assert_equal(data[2], match[1])
+    end
+  end
 end
 
 ##
