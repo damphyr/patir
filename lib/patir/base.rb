9670b71179ddf36b63976a6a0d91c19810586f8c
markuspg <markuspg@users.noreply.github.com>
2021-03-03 00:12:44 +0100

    Enhance documentation within base.rb

--- lib/patir/base.rb
+++ lib/patir/base.rb
@@ -1,41 +1,79 @@
-#  Copyright (c) 2007-2012 Vassilis Rizopoulos. All rights reserved.
+# Copyright (c) 2007-2012 Vassilis Rizopoulos. All rights reserved.
+
 require 'logger'
+
 #This is the base module of the Patir system. It contains some usefull helper methods used by all child projects.
 module Patir
-  #The Patir version used
+  ##
+  # Version information of the Patir gem
   module Version
+    ##
+    # The major version of the Patir gem
     MAJOR=0
+    ##
+    # The minor version of the Patir gem
     MINOR=9
+    ##
+    # The tiny version of the Patir gem
     TINY=0
+    ##
+    # The version information of the Patir gem as a string
     STRING=[ MAJOR, MINOR, TINY ].join( "." )  	
   end
-  #Error thrown usually in initialize methods when missing required parameters
-  #from the initialization hash.
+
+  ##
+  # Exception which is being raised if methods lack required arguments
+  #
+  # Currently this is thrown by ShellCommand only. It's thrown in case the
+  # +params+ hash passed to the initialize method of ShellCommand lacks a +:cmd+
+  # key.
   class ParameterException<RuntimeError
   end
   
+  ##
+  # Modified version of the default log message formatter of Ruby
   class PatirLoggerFormatter<Logger::Formatter
+    ##
+    # Format string defining the format of the created log messages
     Format="[%s] %5s: %s\n"
+
+    ##
+    # Initialize a new PatirLoggerFormatter instance
     def initialize
       @datetime_format="%Y%m%d %H:%M:%S"
     end
     
+    ##
+    # Create and return a log message representing the passed in arguments
+    #
+    # * +severity+ - a value representing the severity of the to be logged
+    #   message
+    # * +time+ - an instance of the Time class giving the time of the log
+    #   message
+    # * +progname+ - _unused_
+    # * +msg+ - the actual message to be logged
     def call severity, time, progname, msg
       Format % [format_datetime(time), severity,
         msg2str(msg)]
     end
   end
-  #Just making Logger usage easier
+
+  ##
+  # Convenience method for quickly setting up a logger
   #
-  #This is for use on top level scripts.
+  # This method allows setting up a logger with sane defaults with a one-liner
+  # and returns the new instance.
   #
-  #It creates a logger just as we want it.
-  #
-  #mode can be
-  # :mute to set the level to FATAL
-  # :silent to set the level to WARN
-  # :debug to set the level to DEBUG. Debug is set also if $DEBUG is true.
-  #The default logger level is INFO
+  # * +filename+ - a string representing of the path to the file which the log
+  #   shall be written to or an +IO+ object. If +nil+ the log is written to
+  #   +stdout+.
+  # * +mode+ - sets the log level and defaults to +Logger::INFO+ if +nil+. If
+  #   $DEBUG is set +Logger::DEBUG+ is forced in any case. Otherwise the
+  #   following arguments are valid:
+  #   * any of the +Logger::Severity+ values
+  #   * +:debug+ - to set the log level to +Logger::DEBUG+
+  #   * +:mute+ - to set the log level to +Logger::FATAL+
+  #   * +:silent+ - to set the log level to +Logger::WARN+
   def self.setup_logger(filename=nil,mode=nil)
     if filename
       logger=Logger.new(filename) 
