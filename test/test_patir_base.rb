9c6b040ffc00031493031806ad392f0a0f3b3dce
markuspg <markuspg@users.noreply.github.com>
2021-03-04 18:49:07 +0100

    Fix RuboCop offenses for base.rb tests

--- test/test_patir_base.rb
+++ test/test_patir_base.rb
@@ -1,25 +1,27 @@
-$:.unshift File.join(File.dirname(__FILE__),"..","lib")
-require "minitest/autorun"
-require 'patir/base.rb'
+# frozen_string_literal: true
 
-class TestBase<Minitest::Test
-  TEMP_LOG="temp.log"
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require "minitest/autorun"
+require "patir/base"
+
+class TestBase < Minitest::Test
+  TEMP_LOG = "temp.log"
   def teardown
-    #clean up 
+    # clean up
     File.delete(TEMP_LOG) if File.exist?(TEMP_LOG)
   end
-  
-  #This is not actually testing anything meaningfull but can be expanded when we learn more about 
-  #the logger
+
+  # This is not actually testing anything meaningfull but can be expanded when we learn more about
+  # the logger
   def test_setup_logger
-    logger=Patir.setup_logger
+    logger = Patir.setup_logger
     refute_nil(logger)
-    logger=Patir.setup_logger(nil,:silent)
+    logger = Patir.setup_logger(nil, :silent)
     refute_nil(logger)
-    logger=Patir.setup_logger("temp.log",:silent)
+    logger = Patir.setup_logger("temp.log", :silent)
     refute_nil(logger)
     assert(File.exist?(TEMP_LOG), "Log file not created")
     logger.close
   end
-  
 end
