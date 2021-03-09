745b3b142dc3c3a31ae3d35157c8d21167f9136c
markuspg <markuspg@users.noreply.github.com>
2021-03-09 19:06:30 +0100

    Fix RuboCop offenses for configuration.rb tests

--- test/test_patir_configuration.rb
+++ test/test_patir_configuration.rb
@@ -1,28 +1,55 @@
-$:.unshift File.join(File.dirname(__FILE__),"..","lib")
+# Copyright (c) 2012-2021 Vassilis Rizopoulos. All rights reserved.
+
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
 require "minitest/autorun"
 
-require 'patir/configuration'
+require "patir/configuration"
+
 module Patir
-  class TestConfigurator<Minitest::Test
+  ##
+  # Verify functionality of the Configurator class
+  class TestConfigurator < Minitest::Test
+    ##
+    # Setup tasks conducted before testcase executions
     def setup
-      @prev_dir=Dir.pwd
+      @prev_dir = Dir.pwd
       Dir.chdir(File.dirname(__FILE__))
     end
+
+    ##
+    # Clean-up tasks conducted after testcase executions
     def teardown
       Dir.chdir(@prev_dir)
     end
+
+    ##
+    # Verify that configuration files can be parsed
     def test_configuration
-      c=Patir::Configurator.new("samples/empty.cfg")
-      assert_equal(c.configuration,c)
-      
-      c=Patir::Configurator.new("samples/chain.cfg")
-      assert_equal(c.configuration,c)
+      c = Patir::Configurator.new("samples/empty.cfg")
+      assert_equal(c.configuration, c)
+
+      c = Patir::Configurator.new("samples/chain.cfg")
+      assert_equal(c.configuration, c)
     end
+
+    ##
+    # Verify that exceptions are raised on invalid configurations
     def test_raise_configuration
-      assert_raises(Patir::ConfigurationException) { Patir::Configurator.new("samples/failed.cfg")}
-      assert_raises(Patir::ConfigurationException) { Patir::Configurator.new("samples/failed_unknown.cfg")}
-      assert_raises(Patir::ConfigurationException) { Patir::Configurator.new("samples/syntax.cfg")}
-      assert_raises(Patir::ConfigurationException) { Patir::Configurator.new("samples/config_fail.cfg")}
+      assert_raises(Patir::ConfigurationException) do
+        Patir::Configurator.new("samples/failed.cfg")
+      end
+      assert_raises(Patir::ConfigurationException) do
+        Patir::Configurator.new("samples/failed_unknown.cfg")
+      end
+      assert_raises(Patir::ConfigurationException) do
+        Patir::Configurator.new("samples/syntax.cfg")
+      end
+      assert_raises(Patir::ConfigurationException) do
+        Patir::Configurator.new("samples/config_fail.cfg")
+      end
     end
   end
 end
