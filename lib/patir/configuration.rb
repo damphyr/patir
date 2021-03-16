06b03523092bd4ae169e107cef67e3453919f703
markuspg <markuspg@users.noreply.github.com>
2021-03-17 00:26:48 +0100

    Fix RuboCop offenses for configuration.rb

--- lib/patir/configuration.rb
+++ lib/patir/configuration.rb
@@ -1,6 +1,9 @@
-# Copyright (c) 2007-2012 Vassilis Rizopoulos. All rights reserved.
+# Copyright (c) 2007-2021 Vassilis Rizopoulos. All rights reserved.
 
-require 'patir/base'
+# frozen_string_literal: true
+
+require "English"
+require "patir/base"
 
 module Patir
   ##
@@ -9,13 +12,13 @@ module Patir
   #
   # This may be caused e.g. by an invalid syntax of the configuration file or
   # unknown configuration directives within it.
-  class ConfigurationException<RuntimeError
+  class ConfigurationException < RuntimeError
   end
-  
+
   ##
   # Configurator is intended as a base class for classes handling configuration
   # files
-  # 
+  #
   # The idea behind the Configurator class is that the developer creates a
   # module that contains methods for all the configuration directives. Then a
   # class can be derived from Configurator which includes the module with the
@@ -45,10 +48,10 @@ module Patir
   #         @name = tool_name
   #       end
   #     end
-  #   
+  #     
   #     class SimpleConfigurator
   #       include SimpleConfiguration
-  #     
+  #       
   #       def configuration
   #         return @name
   #       end
@@ -81,13 +84,13 @@ module Patir
     #   loaded
     # * +logger+ - an optional logger which will log informational and debug
     #   information if given
-    def initialize config_file,logger=nil
-      @logger=logger
-      @logger||=Patir.setup_logger
-      @config_file=config_file
+    def initialize(config_file, logger = nil)
+      @logger = logger
+      @logger ||= Patir.setup_logger
+      @config_file = config_file
       load_configuration(@config_file)
     end
-    
+
     ##
     # Returns self
     #
@@ -96,7 +99,7 @@ module Patir
     def configuration
       return self
     end
-    
+
     ##
     # Loads the configuration from a file
     #
@@ -109,8 +112,8 @@ module Patir
     # These specific files could "include" the general one in the following way:
     #
     #     configuration.load_from_file("first.cfg")
-    def load_from_file filename
-      fnm = File.exist?(filename) ? filename : File.join(@wd,filename)
+    def load_from_file(filename)
+      File.exist?(filename) ? fnm = filename : fnm = File.join(@wd, filename)
       load_configuration(fnm)
     end
 
@@ -123,31 +126,35 @@ module Patir
     # directory containing the file (to make relative references to other
     # configuration files work) and then evaluates the file with the instance
     # itself as context.
-    def load_configuration filename
-      begin 
-        cfg_txt=File.read(filename)
-        @wd=File.expand_path(File.dirname(filename))
-        configuration=self
-        #add the path to the require lookup path to allow require statements in the configuration files
-        $:.unshift File.join(@wd)
-        #evaluate in the working directory to enable relative paths in configuration
-        Dir.chdir(@wd){eval(cfg_txt,binding())}
-        @logger.info("Configuration loaded from #{filename}") if @logger
-      rescue ConfigurationException
-        #pass it on, do not wrap again
-        raise
-      rescue SyntaxError
-        #Just wrap the exception so we can differentiate
-        @logger.debug($!)
-        raise ConfigurationException.new,"Syntax error in the configuration file '#{filename}':\n#{$!.message}"
-      rescue NoMethodError
-        @logger.debug($!)
-        raise ConfigurationException.new,"Encountered an unknown directive in configuration file '#{filename}':\n#{$!.message}"
-      rescue 
-        @logger.debug($!)
-        #Just wrap the exception so we can differentiate
-        raise ConfigurationException.new,"#{$!.message}"
-      end
+    def load_configuration(filename)
+      cfg_txt = File.read(filename)
+      @wd = File.expand_path(File.dirname(filename))
+      configuration = self
+      # Add the path to the require lookup path to allow require statements in
+      # the configuration files
+      $LOAD_PATH.unshift File.join(@wd)
+      # Evaluate in the working directory to enable relative paths in
+      # configuration
+      Dir.chdir(@wd) { eval(cfg_txt, binding) }
+      @logger&.info("Configuration loaded from #{filename}")
+    rescue ConfigurationException
+      # Pass on the exception, do not wrap it again
+      raise
+    rescue SyntaxError
+      # Just wrap the exception so we can differentiate
+      @logger.debug($ERROR_INFO)
+      raise ConfigurationException.new, \
+            "Syntax error in the configuration file '#{filename}':\n" \
+            "#{$ERROR_INFO.message}"
+    rescue NoMethodError
+      @logger.debug($ERROR_INFO)
+      raise ConfigurationException.new, \
+            "Encountered an unknown directive in configuration file '#{filename}':" \
+            "\n#{$ERROR_INFO.message}"
+    rescue StandardError
+      @logger.debug($ERROR_INFO)
+      # Just wrap the exception so we can differentiate
+      raise ConfigurationException.new, $ERROR_INFO.message.to_s
     end
   end
 end
